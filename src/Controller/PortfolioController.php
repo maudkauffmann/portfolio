8a237699192a8b737ac3f649ace3d9e53954d79f
kauf0014 <maud.kauffmann@etudiant.univ-reims.fr>
2026-03-08 23:31:21 +0100

    Ajout modifs 08/03/26

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -13,4 +13,10 @@ class PortfolioController extends AbstractController
     {
         return $this->render('portfolio/index.html.twig');
     }
+
+    #[Route('/mentions-legales', name: 'app_legal')]
+    public function legal(): Response
+    {
+        return $this->render('portfolio/legal.html.twig');
+    }
 }
